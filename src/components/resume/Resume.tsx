cca40e102508ea39cc08914d97e5946d0210aded
Andrew Saifnoorian <andrewsafe@gmail.com>
2026-08-17 16:50:40 -0400

    Restore the pre-2026-resume /resume placeholder page

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -1,30 +1,21 @@
 import { Link } from "react-router-dom";
 import "./resume.css";
-import { FaEnvelope } from "react-icons/fa";
-
-const RESUME_EMAIL = "andrewsafe@gmail.com";
 
 const Resume = () => {
   return (
     <section className="resume_page">
-      <div className="resume_header">
-        <h1>Resume</h1>
-        <p className="text-light">
-          Andrew Saifnoorian &middot; Software Engineer II, JPMorganChase
-        </p>
-        <p className="resume_note text-light">
-          My resume isn't posted publicly, but I'm happy to send a copy. Email me and I'll get it
-          right over.
-        </p>
-        <div className="resume_actions">
-          <a href={`mailto:${RESUME_EMAIL}?subject=Resume request`} className="btn btn-primary">
-            <FaEnvelope /> Email me for my resume
-          </a>
-          <Link to="/" className="btn">
-            Back to portfolio
-          </Link>
-        </div>
-      </div>
+      <h1>Oops... Awkward.</h1>
+      <p>
+        Hey, sorry about that! I'm currently <strong>not</strong> looking for a job, but I
+        appreciate the curiosity!
+      </p>
+      <p>
+        For any serious inquiries, feel free to reach me at{" "}
+        <a href="mailto:andrewsafe@gmail.com">andrewsafe@gmail.com</a>
+      </p>
+      <Link to="/" className="btn btn-primary">
+        Take me back
+      </Link>
     </section>
   );
 };
